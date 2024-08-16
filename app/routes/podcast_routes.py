6ba3938ebd6dcaf9850a1738f0af1b399586f441
KOSASIH <kosasihg88@gmail.com>
2024-08-16 19:58:22 +0700

    Update podcast_routes.py

--- app/routes/podcast_routes.py
+++ app/routes/podcast_routes.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, request, jsonify
 from app.services.podcast_service import PodcastService
-from app.content_based_filtering import ContentBasedFiltering
+from app.hybrid_recommendation import HybridRecommendation
 
 podcast_blueprint = Blueprint('podcast', __name__)
 
-content_based_filtering = ContentBasedFiltering(podcast_episodes)
+hybrid_recommendation = HybridRecommendation(user_podcast_matrix, podcast_episodes)
 
-@podcast_blueprint.route('/podcasts/recommendations/content-based', methods=['GET'])
-def get_content_based_recommendations():
+@podcast_blueprint.route('/podcasts/recommendations/hybrid', methods=['GET'])
+def get_hybrid_recommendations():
+    user_id = request.args.get('user_id')
     podcast_id = request.args.get('podcast_id')
-    recommendations = content_based_filtering.predict_recommendations(podcast_id)
+    recommendations = hybrid_recommendation.predict_recommendations(user_id, podcast_id)
     return jsonify({'recommendations': recommendations})
