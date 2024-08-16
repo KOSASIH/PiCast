57c5d99f3bf55bbcb9f8c77f473dbe17b2d3139d
KOSASIH <kosasihg88@gmail.com>
2024-08-16 19:55:47 +0700

    Create podcast_episodes.py

---
+++ app/podcast_episodes.py
@@ -0,0 +1,7 @@
+# app/podcast_episodes.py
+podcast_episodes = [
+    {'id': 1, 'audio_file': 'episode1.mp3'},
+    {'id': 2, 'audio_file': 'episode2.mp3'},
+    {'id': 3, 'audio_file': 'episode3.mp3'},
+    # ...
+]
